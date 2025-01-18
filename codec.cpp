cb8a82a0470fa2bebb088520b36d0e5c41741679
hejiatai666 <14822811+hejiatai666@user.noreply.gitee.com>
2025-01-18 20:54:46 +0800

    try to fix file duration

--- codec.cpp
+++ codec.cpp
@@ -67,7 +67,7 @@ bool Codec::parse(Atom *trak) {
 			pcm_bytes_per_sample = stsd->readInt(52);
 		} else if(version == 2) {
 			//TODO verify this number!
-			pcm_bytes_per_sample = stsd->readInt(24 + 13*4);
+			pcm_bytes_per_sample = stsd->readInt(24 + 13*4) * 2;
 			assert(pcm_bytes_per_sample < 128);
 		}
 
